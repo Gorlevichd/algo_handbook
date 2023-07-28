eadeb8ec5eb690b510fe3e7b1e3f113d7da44107
Gorlevichd <gorlevichd@gmail.com>
2023-07-28 17:09:46 +0300

    Passed test 11

--- clicks/billboards.py
+++ clicks/billboards.py
@@ -1,53 +1,44 @@
 import logging
+
 logging.basicConfig()
 logger = logging.getLogger("billboards")
 logger.setLevel(logging.DEBUG)
 
-def argsort(seq):
-    return sorted(range(len(seq)), key=seq.__getitem__, reverse=True)
-
-
 def billboard_profit(cost_list, week_list, weeks, n_clients, n_billboards):
     current_week = 0
     billboard_revision = [0] * n_billboards
-    cost_list_argsort = argsort(cost_list)
     logger.debug("cost_list: %s", cost_list)
     logger.debug("week_list: %s", week_list)
-    logger.debug("cost_list_argsort: %s", cost_list_argsort)
     value = 0
-    # Pointer to the current client
-    current_client = 0
+    pointer = 0
+    lists_zip = list(zip(cost_list, week_list))
+    zip_sorted = sorted(lists_zip, key = lambda x: (-x[0], -x[1]))
+    logger.debug(zip_sorted)
     while current_week < weeks:
-        logger.debug("______________    new iter    ________________")
-        logger.debug("current_week: %s", current_week)
+        logger.debug("______________    current_week: %s   ________________", current_week)
         logger.debug("billboard_revision: %s", billboard_revision)
         for billboard_i in range(n_billboards):
+            logger.debug("______________    billboard_i: %s     ______________", billboard_i)
             # If billboard is still occupied, skip it
             if current_week < billboard_revision[billboard_i]:
                 logger.debug("Skipping billboard %s", billboard_i)
                 continue
             # Occupy to the most profitable
-            weeks_opt, cost_opt = (week_list[cost_list_argsort[current_client]],
-                                       cost_list[cost_list_argsort[current_client]])
-            logger.debug("weeks_opt: %s", weeks_opt)
-            weeks_opt_limited  = min(weeks_opt, weeks - current_week)
+            logger.debug("zip_sorted: %s", zip_sorted)
+            cost_opt, week_opt  = zip_sorted[pointer]
+            logger.debug("best_index: %s", (cost_opt, week_opt))
+            weeks_opt_limited = min(week_opt, weeks - current_week)
             logger.debug("weeks_opt_limited: %s", weeks_opt_limited)
             logger.debug("cost_opt: %s", cost_opt)
             billboard_revision[billboard_i] = current_week + weeks_opt_limited
             value += cost_opt * (weeks_opt_limited)
             logger.debug("value: %s", value)
-            current_client += 1
-            # All clients are served
-            if current_client == n_clients:
-                break
-            logger.debug("current_client: %s", current_client)
-        # All clients are served
-        if current_client == n_clients:
-            break
-        # Skip towards next week
+            pointer += 1
+            if pointer == n_clients:
+                return value
+            logger.debug("zip_sorted: %s", zip_sorted)
         current_week = min(billboard_revision)
     return value
-
 
 if __name__ == "__main__":
     n_billboards, n_clients, weeks = map(int, input().split())
@@ -58,9 +49,3 @@ if __name__ == "__main__":
         cost_list.append(cost_i)
         week_list.append(week_i)
     print(billboard_profit(cost_list, week_list, weeks, n_clients, n_billboards))
-
-
-
-
-
-
